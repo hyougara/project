7964e2b506e0f4362ea927fb6bf6c0dabe78cd22
前田優樹 <orzazous2@gmail.com>
2018-10-14 11:47:47 +0900

    Revert "make monologues controller"
    This reverts commit 71b37fb9a04c218b7a6ca62f3a301b9248120042.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'monologues/index'
-  get 'monologues/new'
-  get 'monologues/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
