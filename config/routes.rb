71b37fb9a04c218b7a6ca62f3a301b9248120042
前田優樹 <orzazous2@gmail.com>
2018-10-14 11:34:00 +0900

    make monologues controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'monologues/index'
+  get 'monologues/new'
+  get 'monologues/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
